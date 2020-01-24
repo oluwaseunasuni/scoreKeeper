baec72a882a0cb9baa7a8e386ce2644570d20257
Asuni Oluwaseun <asunioluwaseun@gmail.com>
2020-01-24 18:42:06 +0100

    reset button logic

--- scorekeeper.js
+++ scorekeeper.js
@@ -1,8 +1,9 @@
 var playerOne = document.querySelector("#playerOne");
 var playerTwo = document.querySelector("#playerTwo");
+var resetButton = document.getElementById("reset");
 var playerOnedisplay = document.querySelector("#playerOnedisplay");
-var playerOnescore = 0;
 var playerTwodisplay = document.querySelector("#playerTwodisplay");
+var playerOnescore = 0;
 var playerTwoscore = 0;
 var gameOver = false;
 var winningScore = 5;
@@ -45,10 +46,19 @@ playerTwo.addEventListener("click", function(){
          playerTwoscore++;
          if(playerTwoscore === winningScore){
              gameOver = true;
-
          }
          playerTwodisplay.textContent = playerTwoscore;
 
      }
      
- });
+});
+
+resetButton.addEventListener("click", function(){
+    playerOnescore = 0;
+    playerTwoscore = 0;
+    playerOnedisplay.textContent = 0;
+    playerTwodisplay.textContent = 0;
+    playerOnedisplay.classList.remove("winner");
+    playerTwodisplay.classList.remove("winner");
+    gameOver = false;
+})
