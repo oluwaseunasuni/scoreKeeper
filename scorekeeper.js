ee95aea296ee13979a3994c0b41b3fdeda6b25e5
Asuni Oluwaseun <asunioluwaseun@gmail.com>
2020-01-24 18:12:23 +0100

    color indication logic

--- scorekeeper.js
+++ scorekeeper.js
@@ -1,16 +1,31 @@
- var playerOne = document.querySelector("#playerOne");
- var playerTwo = document.querySelector("#playerTwo");
- var playerOnedisplay = document.querySelector("#playerOnedisplay");
- var playerOnescore = 0;
- var playerTwodisplay = document.querySelector("#playerTwodisplay");
- var playerTwoscore = 0;
+var playerOne = document.querySelector("#playerOne");
+var playerTwo = document.querySelector("#playerTwo");
+var playerOnedisplay = document.querySelector("#playerOnedisplay");
+var playerOnescore = 0;
+var playerTwodisplay = document.querySelector("#playerTwodisplay");
+var playerTwoscore = 0;
+var gameOver = false;
+var winningScore = 5;
 
- playerOne.addEventListener("click", function() {
-     playerOnescore++;
-     playerOnedisplay.textContent = playerOnescore;
- });
+playerOne.addEventListener("click", function() {
+    if(!gameOver){
+       playerOnescore++;
+        if(playerOnescore === winningScore){
+            playerOnedisplay.classList.add("winner");
+            gameOver = true;
+        }
+       playerOnedisplay.textContent = playerOnescore;  
+    }
+});
 
- playerTwo.addEventListener("click", function(){
-     playerTwoscore++;
-     playerTwodisplay.textContent = playerTwoscore;
- });
+playerTwo.addEventListener("click", function(){
+    if(!gameOver){
+        playerTwoscore++;
+        if(playerTwoscore === winningScore){
+            playerTwodisplay.classList.add("winner");
+            gameOver = true;
+        }
+        playerTwodisplay.textContent = playerTwoscore;
+    }
+    
+});
