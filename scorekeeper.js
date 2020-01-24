f0cac8bc72677f5cb4111733c36e49e1decae7b9
Asuni Oluwaseun <asunioluwaseun@gmail.com>
2020-01-24 17:16:26 +0100

    Game Over

--- scorekeeper.js
+++ scorekeeper.js
@@ -4,13 +4,28 @@
  var playerOnescore = 0;
  var playerTwodisplay = document.querySelector("#playerTwodisplay");
  var playerTwoscore = 0;
+ var gameOver = false;
+ var winningScore = 5;
 
  playerOne.addEventListener("click", function() {
-     playerOnescore++;
-     playerOnedisplay.textContent = playerOnescore;
+     if(!gameOver){
+        playerOnescore++;
+        if(playerOnescore === winningScore){
+            gameOver = true;
+        }
+        playerOnedisplay.textContent = playerOnescore;  
+     }
  });
 
  playerTwo.addEventListener("click", function(){
-     playerTwoscore++;
-     playerTwodisplay.textContent = playerTwoscore;
+     if(!gameOver){
+         playerTwoscore++;
+         if(playerTwoscore === winningScore){
+             gameOver = true;
+
+         }
+         playerTwodisplay.textContent = playerTwoscore;
+
+     }
+     
  });
