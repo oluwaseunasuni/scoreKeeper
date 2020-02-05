6108cd0a119cafac6ea6337c9548f522f07c0395
Asuni Oluwaseun <asunioluwaseun@gmail.com>
2020-02-05 15:55:18 +0100

    fixed bug, added reset function

--- scorekeeper.js
+++ scorekeeper.js
@@ -4,7 +4,6 @@ var resetButton = document.getElementById("reset");
 var playerOnedisplay = document.querySelector("#playerOnedisplay");
 var playerTwodisplay = document.querySelector("#playerTwodisplay");
 var numInput = document.querySelector("input");
-var p = document.querySelector("p");
 var winningScoreDisplay = document.querySelector("p span");
 var playerOnescore = 0;
 var playerTwoscore = 0;
@@ -22,16 +21,6 @@ playerOne.addEventListener("click", function() {
     }
 });
 
- playerOne.addEventListener("click", function() {
-     if(!gameOver){
-        playerOnescore++;
-        if(playerOnescore === winningScore){
-            gameOver = true;
-        }
-        playerOnedisplay.textContent = playerOnescore;  
-     }
- });
-
 playerTwo.addEventListener("click", function(){
     if(!gameOver){
         playerTwoscore++;
@@ -40,33 +29,26 @@ playerTwo.addEventListener("click", function(){
             gameOver = true;
         }
         playerTwodisplay.textContent = playerTwoscore;
-    }
-    
-});
-
- playerTwo.addEventListener("click", function(){
-     if(!gameOver){
-         playerTwoscore++;
-         if(playerTwoscore === winningScore){
-             gameOver = true;
-         }
-         playerTwodisplay.textContent = playerTwoscore;
-
-     }
-     
+    } 
 });
 
 resetButton.addEventListener("click", function(){
+    reset();
+});
+
+function reset(){
     playerOnescore = 0;
     playerTwoscore = 0;
     playerOnedisplay.textContent = 0;
     playerTwodisplay.textContent = 0;
-    playerOnedisplay.classList.remove("winner");
-    playerTwodisplay.classList.remove("winner");
+    playerOnedisplay.classList.remove("winner");        playerTwodisplay.classList.remove("winner");
     gameOver = false;
-});
 
+}
+
+//updates number of rounds according to the value entered in the input
 numInput.addEventListener("change", function(){
     winningScoreDisplay.textContent = numInput.value;
-    winningScore = Number(numInput.value);   
+    winningScore = Number(numInput.value);
+    reset (); 
 });
